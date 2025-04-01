3fa4b4a5fe81dd142da4e78a9728a6cb95db6213
MrPapricot <118524942+MrPapricot@users.noreply.github.com>
2025-04-01 18:15:21 +0700

    Delete
    Удалил лишнее

--- src/RestorePassword.tsx
+++ src/RestorePassword.tsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
 import './App.css'
 import './styles.css'
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { emailResult, passwordResult, checkEmail, checkPassword } from './utilities';
 
 
 function RestorePassword() {
