d8b7c88eb66df6d57ce4beb19de7586145975c27
MrFromer <artem.safren.123@gmail.com>
2025-04-13 20:40:22 +0700

    Создал модель Гида (User_Guide) и создал модель Экскурсии (post_excurtion) +  добавил две миграции на каждую из моделей

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -4,9 +4,13 @@ __all__ = (
     "db_helper",
     "Product",
     "User",
+    "User_Guide",
+    "Post_Excurtion",
 )
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
 from .product import Product
 from .user import User
+from .user_guide import User_Guide
+from .post import Post_Excurtion
---
+++ core/models/post.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Text, ForeignKey
+
+from .base import Base
+
+
+class Post_Excurtion(Base):
+    title: Mapped[str] = mapped_column(String(100))
+    body: Mapped[str] = mapped_column(
+        Text,
+        default="",
+        server_default="",
+    )
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("user_guides.id"),
+    )
+    
