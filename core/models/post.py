80eb1d9496d4aef4c9fa6655160bed92715ca401
MrFromer <artem.safren.123@gmail.com>
2025-04-22 18:24:33 +0700

    add column: city, has_children, date, persons_count, date, status in post table and instead of body make description

--- core/models/post.py
+++ core/models/post.py
@@ -1,22 +1,43 @@
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column, validates
+from sqlalchemy import Boolean, Integer, String, Text, DateTime, CheckConstraint
+from datetime import datetime, timezone
 
 from .base import Base
 from .mixins import UserGuideRelations
 
-class Post_Excurtion(UserGuideRelations,Base):
+
+class Post_Excurtion(UserGuideRelations, Base):
     _user_guide_back_populates = "posts"
     _user_back_populates = "posts"
 
+    city: Mapped[str] = mapped_column(String(30))
     title: Mapped[str] = mapped_column(String(100))
-    body: Mapped[str] = mapped_column(
-        Text,
-        default="",
-        server_default="",
+    description: Mapped[str] = mapped_column(Text, default="", server_default="")
+    has_childen: Mapped[bool] = mapped_column(
+        Boolean, default=False, server_default="false"
+    )
+    persons_count: Mapped[int] = mapped_column(
+        Integer, nullable=False, default=1, server_default="1"
+    )
+    date: Mapped[datetime] = mapped_column( # время храниться в UTC
+        DateTime, nullable=False
+    )  
+    status: Mapped[str] = mapped_column(
+        String(20), nullable=False, default="change", server_default="change"
     )
 
-    
+    __table_args__ = (
+        CheckConstraint(# Для SQLite проверки на дату из прошлого
+            "date >= datetime('now')", name="date_not_in_past"
+        ),  
+        CheckConstraint(  # проверка, что статус экскурсии один из этих 3-х
+            "status IN ('approved', 'disapproved', 'change')",
+            name="check_status_values",
+        ),
+    )
 
-    
-  
-    
+    @validates("date")  # python проверка на дату из прошлого
+    def validate_date(self, key, value):
+        if value < datetime.now(timezone.utc).replace(tzinfo=None):
+            raise ValueError("Дата экскурсии должна быть в будущем")
+        return value
