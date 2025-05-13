04f22045c9c19cacceccdc816f6dd44db6a53a8c
бобпоп <106816769+Estuss@users.noreply.github.com>
2025-05-13 18:58:06 +0700

    Добавил настройки профиля, сделал выпадающие окна, там пока плейсхолдер

--- android/app/src/main/java/ru/tbank/excursions/ActivityProfileSettings.kt
+++ android/app/src/main/java/ru/tbank/excursions/ActivityProfileSettings.kt
@@ -1,10 +1,12 @@
 package ru.tbank.excursions
 
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
 
 class ActivityProfileSettings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +14,21 @@ class ActivityProfileSettings : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile_settings)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val changeNameButton = findViewById<FrameLayout>(R.id.fr)
+        val changePasswordButton = findViewById<FrameLayout>(R.id.change_password)
+
+        changeNameButton.setOnClickListener {
+            val view: View = layoutInflater.inflate(R.layout.item_excursion_card, null)
+            val dialog = BottomSheetDialog(this)
+            dialog.setContentView(view)
+            dialog.show()
+        }
+
+        changePasswordButton.setOnClickListener {
+            val view: View = layoutInflater.inflate(R.layout.item_excursion_card, null)
+            val dialog = BottomSheetDialog(this)
+            dialog.setContentView(view)
+            dialog.show()
         }
     }
 }
--- android/app/src/main/java/ru/tbank/excursions/ApiInterface.kt
+++ android/app/src/main/java/ru/tbank/excursions/ApiInterface.kt
@@ -1,11 +1,9 @@
 package ru.tbank.excursions
 
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface ApiInterface {
